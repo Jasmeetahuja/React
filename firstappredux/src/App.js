8fe15b9432ec824dab9691c9fda83dbbb3561324
JasmeetAhuja <jassideep013@gmail.com>
2019-06-20 22:00:41 +0530

    jshj

--- firstappredux/src/App.js
+++ firstappredux/src/App.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
+import { connect } from 'react-redux';
+import * as actions from './actions';
+
 
 class App extends Component {
+
+    componentWillMount(){
+        this.props.moviesList()
+    }
+
+    renderList = (movies) => {
+        if(movies){
+        return movies.map((movie)=>{
+            return (
+                <div>{movie.name}</div>
+            )
+        })
+    }
+    }
+
+
     render(){
         return(
-            <div> Hello world </div>
+            <div>
+                {this.renderList(this.props.movies)}
+            </div>
         )
     }
 }
 
-export default App;
+function mapStateToProps(state){
+    console.log(state)
+    return {
+        movies:state.movies
+    }
+
+}
+
+export default connect(mapStateToProps,actions)(App);
