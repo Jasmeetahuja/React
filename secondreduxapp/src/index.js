e7fdf6555699a27af11d042f1e40e8679c903d60
JasmeetAhuja <jassideep013@gmail.com>
2019-06-21 23:13:15 +0530

    b

---
+++ secondreduxapp/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+import App from './components/app';
+import Car from './components/car';
+
+
+
+ReactDOM.render(
+    <BrowserRouter>
+    <div>
+        <Route exact path="/" component={App}></Route>
+        <Route path="/car/:id" component={Car}></Route>
+    </div>
+    </BrowserRouter>
+    , document.getElementById('root'));
+
+
